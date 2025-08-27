f85889c7ab2f9ca0f100c5ea7b463f5ed944ad76
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-08-27 14:00:33 -0400

    CE-4603 CTX Support For ToxCast Model #140 (#141)
    Co-authored-by: BDIRKS <BDIRKS@host.docker.internal>

---
+++ src/main/java/gov/epa/ccte/api/bioactivity/web/rest/BioactivityModelResource.java
@@ -0,0 +1,33 @@
+package gov.epa.ccte.api.bioactivity.web.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import gov.epa.ccte.api.bioactivity.domain.BioactivityModel;
+import gov.epa.ccte.api.bioactivity.repository.BioactivityModelRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class BioactivityModelResource implements BioactivityModelApi{
+
+	private final BioactivityModelRepository repository;
+	
+	public BioactivityModelResource(BioactivityModelRepository repository) {
+		this.repository = repository;
+	}
+	
+    @Override
+    public @ResponseBody
+    List<BioactivityModel> getBioactivityModelByDtxsid(String dtxsid) {
+
+        log.debug("Find Bioactivity Model for dtxsid = {}", dtxsid);
+
+        List<BioactivityModel> data = repository.findByDtxsid(dtxsid);
+
+        return data;
+    }
+
+}
