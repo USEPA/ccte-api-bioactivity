37dcb17c7420d037f9d456b019fcfe0c871976b8
SrikanthDeevi <deevi.srikanth@epa.gov>
2026-06-25 11:43:49 -0400

    Ce-5114 used application batch size

--- src/main/java/gov/epa/ccte/api/bioactivity/web/rest/AssayResource.java
+++ src/main/java/gov/epa/ccte/api/bioactivity/web/rest/AssayResource.java
@@ -24,7 +24,7 @@ public class AssayResource implements AssayApi {
     private final BioactivityScRepository bioactivityScRepository;
 
 
-    @Value("${bioactivity.batch.size:200}")
+    @Value("${application.batch-size:200}")
     private Integer batchSize;
 
     public AssayResource(AssayAnnotationRepository annotationRepository,
