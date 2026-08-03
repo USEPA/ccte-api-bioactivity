935df86d463329e1ab1e0fb9ff8d1fe7f5a1677a
SrikanthDeevi <deevi.srikanth@epa.gov>
2026-08-03 14:54:15 -0400

    CE-5278 Removed unused imports

--- src/test/java/gov/epa/ccte/api/bioactivity/repository/AnalyticalQCRepositoryTest.java
+++ src/test/java/gov/epa/ccte/api/bioactivity/repository/AnalyticalQCRepositoryTest.java
@@ -2,7 +2,6 @@ package gov.epa.ccte.api.bioactivity.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
