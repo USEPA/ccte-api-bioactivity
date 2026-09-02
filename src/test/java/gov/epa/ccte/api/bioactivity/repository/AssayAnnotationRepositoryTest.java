019dfd58da95b78f4c9e3a431bb434d4c4b0bd08
SrikanthDeevi <deevi.srikanth@epa.gov>
2026-09-02 10:17:42 -0400

    CE-5311- Changes to use embedded postgres instead of skipping tests

--- src/test/java/gov/epa/ccte/api/bioactivity/repository/AssayAnnotationRepositoryTest.java
+++ src/test/java/gov/epa/ccte/api/bioactivity/repository/AssayAnnotationRepositoryTest.java
@@ -1,8 +1,8 @@
 package gov.epa.ccte.api.bioactivity.repository;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,11 +12,13 @@ import gov.epa.ccte.api.bioactivity.projection.assay.*;
 
 import javax.sql.DataSource;
 
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.EMBEDDED;
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES;
 import static org.assertj.core.api.Assertions.*;
 
-@DataJpaTest
+@DataJpaTest(properties = "spring.jpa.properties.hibernate.connection.provider_disables_autocommit=false")
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureEmbeddedDatabase(type = POSTGRES, provider = EMBEDDED)
 public class AssayAnnotationRepositoryTest {
 
     private static final String POSTGRES_ONLY_MESSAGE =
